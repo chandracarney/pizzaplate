3249572b7909c868277d5c2c3d821c50d0d5db4e
Chandra Carney <chandracarney@me.com>
2015-06-13 11:52:22 -0600

    Grammar

--- pizza_plate_test.rb
+++ pizza_plate_test.rb
@@ -9,7 +9,7 @@ class PizzaPlateTest < Minitest::Test
     assert_equal "Your pizza does not fit.", pizza.pizza_fits
   end
 
-  def test_pizza_does_fits_on_plate
+  def test_pizza_fits_on_plate
     pizza = PizzaPlate.new(2, 8)
     assert_equal "Your pizza fits on the plate!", pizza.pizza_fits
   end
